b2ccd95628c211b5b4e9fe517e49a81158194413
Abdul Wahab Shah <wahab.shah23@gmail.com>
2018-01-26 10:41:30 +0500

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A test Recipe 1', 'This is a Test Recipe 1', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    new Recipe('A test Recipe 2', 'This is a Test Recipe 2', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A test Recipe 3', 'This is a Test Recipe 3', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+/* Commented Code is created automatically and assigneed to
+newly created properties
+*/
+
+
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    constructor( public name: string, public amount: number) {
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
